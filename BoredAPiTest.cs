51e90a606720804f0310f066fae9a8e602fb56b1
swetapadma <133876345+swetapadma-1987@users.noreply.github.com>
2023-05-22 01:12:38 +0530

    Add files via upload

---
+++ BoredAPiTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Api;
+using Services.BoredAPI;
+using System.Threading.Tasks;
+
+namespace Apitest2.BoredApi
+{
+
+    public class BoredAPiTest
+    {
+        [Test]
+        [Category("Tests")]
+        
+        public async Task API_Test() 
+        {
+
+            Services.BoredAPI.BoredApi test = new Services.BoredAPI.BoredApi();
+            var response = await test.Get_200Ok();
+            Assert.IsNotNull(response);
+            
+                  }
+
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ BoredApi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Diagnostics;
+
+namespace Services.BoredAPI;
+
+
+public class BoredApi
+{
+    private HttpClient restClient = new HttpClient();
+    private string URI = "https://www.boredapi.com/api/activity";
+    
+    private readonly Dictionary<string, object> _queryparams = new Dictionary<string, object>()
+    {
+        ["type"] = "social",
+        ["participants"] = "2"
+    };
+    private readonly Dictionary<string, string> _headers = new Dictionary<string, string>() 
+    {
+        ["Accept"] = "*/*",
+        ["Accept-Encoding"] = "gzip, deflate, br",
+        ["Content-Type"] = "application/json",
+        ["Connection"] = "keep-alive"
+
+    };
+    public async Task<string> Get_200Ok() {
+        restClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+        restClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept","*/*");
+        restClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, br");
+        restClient.DefaultRequestHeaders.TryAddWithoutValidation("Connection", "keep-alive");
+
+        var response = await restClient.GetAsync(URI);
+        var reasonPhrase = response.ReasonPhrase.ToString();
+        return reasonPhrase;
+
+    }
+
+};
